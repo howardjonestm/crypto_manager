05a0a4a4294c18e97b5556b02251adf0d75877e5
howardjonestm <howard.j@live.co.uk>
2018-01-04 12:20:03 +0000

    changes

--- dump.sql
+++ dump.sql
@@ -37,7 +37,7 @@ CREATE TABLE `balances` (
 
 LOCK TABLES `balances` WRITE;
 /*!40000 ALTER TABLE `balances` DISABLE KEYS */;
-INSERT INTO `balances` VALUES (1,1,0,0),(2,2,10,10),(3,3,0,0),(4,4,0,0),(5,5,0,0),(6,6,0,0),(7,7,0,0),(8,8,0,0),(9,9,162,-10),(10,10,46,34),(11,11,0,0),(12,12,0,0),(13,13,125.33330000000001,-50),(14,14,0,0),(15,15,87.6665446,-100),(16,16,0,0),(17,17,950,0),(18,18,258.344,109.4334),(19,19,0,0);
+INSERT INTO `balances` VALUES (1,1,0,0),(2,2,10,10),(3,3,0,0),(4,4,0,0),(5,5,0,0),(6,6,0,0),(7,7,0,0),(8,8,0,0),(9,9,162,-10),(10,10,46,34),(11,11,0,0),(12,12,0,0),(13,13,100,-50),(14,14,0,0),(15,15,87.6665446,-100),(16,16,0,0),(17,17,950,0),(18,18,270.9106,95.9779),(19,19,0,0);
 /*!40000 ALTER TABLE `balances` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -60,7 +60,7 @@ CREATE TABLE `group_membership` (
 
 LOCK TABLES `group_membership` WRITE;
 /*!40000 ALTER TABLE `group_membership` DISABLE KEYS */;
-INSERT INTO `group_membership` VALUES (1,28),(6,28),(1,27),(13,NULL),(13,NULL),(13,41),(14,43),(1,43),(3,43),(14,46),(14,48),(15,49),(3,49),(NULL,NULL),(NULL,NULL),(NULL,NULL),(16,50),(3,50),(17,54),(3,53),(18,57),(18,58),(16,57),(3,57);
+INSERT INTO `group_membership` VALUES (1,28),(6,28),(1,27),(13,NULL),(13,NULL),(13,41),(14,43),(1,43),(3,43),(14,46),(14,48),(15,49),(3,49),(NULL,NULL),(NULL,NULL),(NULL,NULL),(16,50),(3,50),(17,54),(3,53),(18,57),(18,58),(18,59),(3,NULL),(18,60),(3,60),(3,59),(3,57),(16,57);
 /*!40000 ALTER TABLE `group_membership` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +88,7 @@ CREATE TABLE `group_transaction` (
 
 LOCK TABLES `group_transaction` WRITE;
 /*!40000 ALTER TABLE `group_transaction` DISABLE KEYS */;
-INSERT INTO `group_transaction` VALUES ('Newdffdg','1234@1234',1513689436,12,12,841.616,18338.2),('','1234@1234',1513689684,0,0,842.659,18411.1),('6@6 Leeds the way','6@6',1513689867,-13.44,12.22,842.659,18411.1),('Judith\'s group','judith@judith',1513714594,-12.44444,12.2222,846.944,18168.3),('Melissa\'s group','Melissa@Melissa',1514135172,-10900,1000,665.249,13472.6),('This is melissa\'s group','Melissa@Melissa',1514463196,12.22,100,713.061,14376.7),('This a new group','56@56',1514556302,-100,100,751.931,14528.4),('Howard\'s group','56@56',1514914843,0,0,2.41471,14021),('Howard\'s group','56@56',1514916395,-1222,12,2.43757,14015.6),('sdffdf','56@56',1514918307,122,-12,2.42106,14549),('sdffdf','56@56',1514918338,122,-12,2.42106,14549),('Howard\'s group','56@56',1514918370,-12,12,2.39224,14549),('sdffdf','56@56',1514918449,122,122,2.39224,14656),('Howard\'s group','56@56',1514919022,23,23,897.363,14743.7),('Howard\'s group','56@56',1514919049,122,122,893.337,14817.8),('Select','56@56',1514919175,-122,122,893.337,14817.8),('Select','56@56',1514919177,0,0,893.337,14817.8),('Select','56@56',1514919183,-233,-233,893.337,14817.8),('sdffdf','56@56',1514925621,0,12,872.567,15190.6),('sdffdf','56@56',1514939167,12.3333,12.233,889.876,15100.6);
+INSERT INTO `group_transaction` VALUES ('Newdffdg','1234@1234',1513689436,12,12,841.616,18338.2),('','1234@1234',1513689684,0,0,842.659,18411.1),('6@6 Leeds the way','6@6',1513689867,-13.44,12.22,842.659,18411.1),('Judith\'s group','judith@judith',1513714594,-12.44444,12.2222,846.944,18168.3),('Melissa\'s group','Melissa@Melissa',1514135172,-10900,1000,665.249,13472.6),('This is melissa\'s group','Melissa@Melissa',1514463196,12.22,100,713.061,14376.7),('This a new group','56@56',1514556302,-100,100,751.931,14528.4),('Howard\'s group','56@56',1514914843,0,0,2.41471,14021),('Howard\'s group','56@56',1514916395,-1222,12,2.43757,14015.6),('sdffdf','56@56',1514918307,122,-12,2.42106,14549),('sdffdf','56@56',1514918338,122,-12,2.42106,14549),('Howard\'s group','56@56',1514918370,-12,12,2.39224,14549),('sdffdf','56@56',1514918449,122,122,2.39224,14656),('Howard\'s group','56@56',1514919022,23,23,897.363,14743.7),('Howard\'s group','56@56',1514919049,122,122,893.337,14817.8),('Select','56@56',1514919175,-122,122,893.337,14817.8),('Select','56@56',1514919177,0,0,893.337,14817.8),('Select','56@56',1514919183,-233,-233,893.337,14817.8),('sdffdf','56@56',1514925621,0,12,872.567,15190.6),('sdffdf','56@56',1514939167,12.3333,12.233,889.876,15100.6),('A modal added group','56@56',1515008825,12,12,939.615,15035.5),('Howard\'s group','56@56',1515062142,0,0,960.424,14908.3),('A team of keen investors','56@56',1515063909,0,-12,967.75,14986.5);
 /*!40000 ALTER TABLE `group_transaction` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -105,7 +105,7 @@ CREATE TABLE `groups` (
   `admin_user` int(11) NOT NULL,
   `group_description` varchar(300) NOT NULL,
   PRIMARY KEY (`group_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -114,7 +114,7 @@ CREATE TABLE `groups` (
 
 LOCK TABLES `groups` WRITE;
 /*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-INSERT INTO `groups` VALUES (1,'12121',9,'dfgfdfdgdg'),(2,'Project uno',10,'A group of friends investing in bitcoin together '),(3,'Project dos',10,'A group of friends investing in ethmereu together '),(4,'bhbhbh',11,'bhbhbh'),(5,'New dfdsf',11,'fsgsfdg'),(6,'New dfdsfyrty',11,'fsgsfdgttr'),(7,'My new projecy',11,'A quick and easy description '),(26,'sfsddsgf',12,'sdfgfsdsfg'),(27,'sfsdfgs',12,'fsgfsdgdg'),(28,'qwert',12,'qwert'),(41,'NewGroupForFUn',13,'A new investment group'),(43,'6@6 Leeds the way',14,'A selection of people who know what they\'re doing '),(46,'blahBlah',14,'fwerrewrrewerw'),(48,'trereert',14,'gddfgdggfd'),(49,'Judith\'s group',15,'blah blah blah'),(50,'This is a new group',16,'blah blah blah'),(53,'This is melissa\'s group',17,'Melissa work friends investment group '),(54,'Another group by Melissa',17,'Things are happening here '),(57,'Howard\'s group',18,'A new investment group'),(58,'sdffdf',18,'sdfds');
+INSERT INTO `groups` VALUES (1,'12121',9,'dfgfdfdgdg'),(2,'Project uno',10,'A group of friends investing in bitcoin together '),(3,'Project dos',10,'A group of friends investing in ethmereu together '),(4,'bhbhbh',11,'bhbhbh'),(5,'New dfdsf',11,'fsgsfdg'),(6,'New dfdsfyrty',11,'fsgsfdgttr'),(7,'My new projecy',11,'A quick and easy description '),(26,'sfsddsgf',12,'sdfgfsdsfg'),(27,'sfsdfgs',12,'fsgfsdgdg'),(28,'qwert',12,'qwert'),(41,'NewGroupForFUn',13,'A new investment group'),(43,'6@6 Leeds the way',14,'A selection of people who know what they\'re doing '),(46,'blahBlah',14,'fwerrewrrewerw'),(48,'trereert',14,'gddfgdggfd'),(49,'Judith\'s group',15,'blah blah blah'),(50,'This is a new group',16,'blah blah blah'),(53,'This is melissa\'s group',17,'Melissa work friends investment group '),(54,'Another group by Melissa',17,'Things are happening here '),(57,'Howard\'s group',18,'A new investment group'),(58,'sdffdf',18,'sdfds'),(59,'A modal added group',18,'This is a new group added by modal'),(60,'A team of keen investors ',18,'We want to make mega money ');
 /*!40000 ALTER TABLE `groups` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -131,7 +131,7 @@ CREATE TABLE `logged_in_member` (
   `session_id` char(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `token` char(128) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -140,7 +140,7 @@ CREATE TABLE `logged_in_member` (
 
 LOCK TABLES `logged_in_member` WRITE;
 /*!40000 ALTER TABLE `logged_in_member` DISABLE KEYS */;
-INSERT INTO `logged_in_member` VALUES (8,10,'4f441c1e221c478af630278e85b57f16','b936cee86c9f87aa5d3c6f2e84cb5a4239a5fe50480a6ec66b70ab5b1f4ac6730c6c515421b327ec1d69402e53dfb49ad7381eb067b338fd7b0cb22247225d47'),(9,11,'4f441c1e221c478af630278e85b57f16','b936cee86c9f87aa5d3c6f2e84cb5a4239a5fe50480a6ec66b70ab5b1f4ac6730c6c515421b327ec1d69402e53dfb49ad7381eb067b338fd7b0cb22247225d47'),(15,15,'4f441c1e221c478af630278e85b57f16','b936cee86c9f87aa5d3c6f2e84cb5a4239a5fe50480a6ec66b70ab5b1f4ac6730c6c515421b327ec1d69402e53dfb49ad7381eb067b338fd7b0cb22247225d47');
+INSERT INTO `logged_in_member` VALUES (8,10,'4f441c1e221c478af630278e85b57f16','b936cee86c9f87aa5d3c6f2e84cb5a4239a5fe50480a6ec66b70ab5b1f4ac6730c6c515421b327ec1d69402e53dfb49ad7381eb067b338fd7b0cb22247225d47'),(9,11,'4f441c1e221c478af630278e85b57f16','b936cee86c9f87aa5d3c6f2e84cb5a4239a5fe50480a6ec66b70ab5b1f4ac6730c6c515421b327ec1d69402e53dfb49ad7381eb067b338fd7b0cb22247225d47'),(15,15,'4f441c1e221c478af630278e85b57f16','b936cee86c9f87aa5d3c6f2e84cb5a4239a5fe50480a6ec66b70ab5b1f4ac6730c6c515421b327ec1d69402e53dfb49ad7381eb067b338fd7b0cb22247225d47'),(25,13,'77e43e694977b9a2d9b082314f436d3f','b936cee86c9f87aa5d3c6f2e84cb5a4239a5fe50480a6ec66b70ab5b1f4ac6730c6c515421b327ec1d69402e53dfb49ad7381eb067b338fd7b0cb22247225d47');
 /*!40000 ALTER TABLE `logged_in_member` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -179,4 +179,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-01-03 11:47:31
+-- Dump completed on 2018-01-04 12:19:40
